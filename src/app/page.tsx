6036241166522ce5e3bc28c576f70454ff588484
Mang Meassreynuon <jordanwatts7946@gmail.com>
2025-08-01 19:10:17 -0700

    Added a line item to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
           <li>🎓 Expert Khmer Tutors</li>
           <li>🎮 Coming Soon: Interactive Language Game</li>
           <li>🌐 Convenient Online Scheduling & Payments</li>
+          <li>🌐 More Stuff</li>
         </ul>
       </section>
     </main>
